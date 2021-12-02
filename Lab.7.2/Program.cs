4993e0fcfb50126f5608ddb8e1a491b00acea26d
Евгений Силенков <ensilenkov@gmail.com>
2021-12-02 19:18:54 +0700

    Добавьте файлы проекта.

---
+++ Lab.7.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab._7._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int s, v;
+            Cube(a, out s, out v);
+            Console.WriteLine(s);
+            Console.WriteLine(v);
+            Console.ReadKey();
+        }
+        static void Cube(int a, out int s, out int v)
+        {
+            s = a * a * 6;
+            v = a * a * a;
+            
+        }
+    }
+}
---
+++ Lab.7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab._7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            int b1 = Convert.ToInt32(Console.ReadLine());
+            int c1 = Convert.ToInt32(Console.ReadLine());
+            int a2 = Convert.ToInt32(Console.ReadLine());
+            int b2 = Convert.ToInt32(Console.ReadLine());
+            int c2 = Convert.ToInt32(Console.ReadLine());
+            double max = SqTr(a1, b1, c1, a2, b2, c2);
+
+            Console.WriteLine(max);
+            Console.ReadKey();
+        }
+        static double SqTr(int a1, int b1, int c1, int a2, int b2, int c2)
+        {
+            double p1 = (a1 + b1 + c1) / 2;
+            double s1 = Math.Sqrt(p1 * (p1 - a1) * (p1 - b1) * (p1 - c1));
+            double p2 = (a2 + b2 + c2) / 2;
+            double s2 = Math.Sqrt(p2 * (p2 - a2) * (p2 - b2) * (p2 - c2));
+            double max = Math.Max(s1, s2);
+            return max;
+        }
+    }
+}
